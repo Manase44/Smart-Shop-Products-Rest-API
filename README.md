c09e714ac444e4cc4b77c9aed5186479c41a4019
Manase44 <fikopersempre@gmail.com>
2024-06-23 17:27:59 +0300

    add the .env file and initialized the README file

---
+++ README.md
@@ -0,0 +1,6 @@
+# How To Run This Project
+## Step 1: Clone the Repo to your locat storage
+## Step 2: Install packages
+## Step 3: Connect to a postgres database
+## step 4: Launch your favorite API platform
+## Step 5: Send your HTTP requests
--- db/db.config.js
+++ db/db.config.js
@@ -1,12 +1,14 @@
 import pkg from "pg";
+import { config } from "dotenv";
 const { Pool } = pkg;
 
+config()
 const pool = new Pool({
-  user: "postgres",
-  password: "manfikohdb",
-  host: "localhost",
-  post: "5432",
-  database: "productapi",
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  database: process.env.POSTGRES_DB,
 });
 
 export default pool;
